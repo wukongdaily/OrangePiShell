d7dbb40cc403c2357ba5273fc2afd7b074914003
wukongdaily <2666180@gmail.com>
2024-04-26 08:53:24 +0800

    Update pi.sh

--- zero3/pi.sh
+++ zero3/pi.sh
@@ -68,59 +68,119 @@ update_system_packages() {
 }
 
 # 安装文件管理器
-install_filemanager() {
-    trap 'echo -e "Aborted, error $? in command: $BASH_COMMAND"; trap ERR; return 1' ERR
-    install_path="/usr/local/bin"
-    filemanager_arch="arm64"
-    filemanager_os="linux"
-    filemanager_bin="filebrowser"
-    filemanager_dl_ext=".tar.gz"
-    sudo_cmd="sudo"
-    # Fall back to /usr/bin if necessary
-    if [[ ! -d $install_path ]]; then
-        install_path="/usr/bin"
-    fi
-    unamem="$(uname -m)"
-    ########################
-    # Download and extract #
-    ########################
-    echo "Downloading File Browser for $filemanager_os/$filemanager_arch..."
-    net_getter="curl -fsSL"
-    filemanager_file="${filemanager_os}-$filemanager_arch-filebrowser$filemanager_dl_ext"
-    filemanager_tag="$(${net_getter} https://api.github.com/repos/filebrowser/filebrowser/releases/latest | grep -o '"tag_name": ".*"' | sed 's/"//g' | sed 's/tag_name: //g')"
-    filemanager_url="${proxy}https://github.com/filebrowser/filebrowser/releases/download/$filemanager_tag/$filemanager_file"
-    echo "$filemanager_url"
+# 源自 https://filebrowser.org/installation
+install_filemanager()
+{
+	trap 'echo -e "Aborted, error $? in command: $BASH_COMMAND"; trap ERR; return 1' ERR
+	filemanager_os="unsupported"
+	filemanager_arch="unknown"
+	install_path="/usr/local/bin"
 
-    # Use $PREFIX for compatibility with Termux on Android
-    rm -rf "$PREFIX/tmp/$filemanager_file"
+	# Termux on Android has $PREFIX set which already ends with /usr
+	if [[ -n "$ANDROID_ROOT" && -n "$PREFIX" ]]; then
+		install_path="$PREFIX/bin"
+	fi
 
-    ${net_getter} "$filemanager_url" >"$PREFIX/tmp/$filemanager_file"
+	# Fall back to /usr/bin if necessary
+	if [[ ! -d $install_path ]]; then
+		install_path="/usr/bin"
+	fi
 
-    echo "Extracting..."
-    case "$filemanager_file" in
-    *.zip) unzip -o "$PREFIX/tmp/$filemanager_file" "$filemanager_bin" -d "$PREFIX/tmp/" ;;
-    *.tar.gz) tar -xzf "$PREFIX/tmp/$filemanager_file" -C "$PREFIX/tmp/" "$filemanager_bin" ;;
-    esac
-    chmod +x "$PREFIX/tmp/$filemanager_bin"
+	# Not every platform has or needs sudo (https://termux.com/linux.html)
+	((EUID)) && [[ -z "$ANDROID_ROOT" ]] && sudo_cmd="sudo"
 
-    echo "Putting filemanager in $install_path (may require password)"
-    $sudo_cmd mv "$PREFIX/tmp/$filemanager_bin" "$install_path/$filemanager_bin"
-    if setcap_cmd=$(PATH+=$PATH:/sbin type -p setcap); then
-        $sudo_cmd $setcap_cmd cap_net_bind_service=+ep "$install_path/$filemanager_bin"
-    fi
-    $sudo_cmd rm -- "$PREFIX/tmp/$filemanager_file"
+	#########################
+	# Which OS and version? #
+	#########################
 
-    if type -p $filemanager_bin >/dev/null 2>&1; then
-        green "Successfully installed"
-        trap ERR
-        start_filemanager
-        return 0
-    else
-        echo "Something went wrong, File Browser is not in your path"
-        trap ERR
-        return 1
-    fi
+	filemanager_bin="filebrowser"
+	filemanager_dl_ext=".tar.gz"
 
+	# NOTE: `uname -m` is more accurate and universal than `arch`
+	# See https://en.wikipedia.org/wiki/Uname
+	unamem="$(uname -m)"
+	case $unamem in
+	*aarch64*)
+		filemanager_arch="arm64";;
+	*64*)
+		filemanager_arch="amd64";;
+	*86*)
+		filemanager_arch="386";;
+	*armv5*)
+		filemanager_arch="armv5";;
+	*armv6*)
+		filemanager_arch="armv6";;
+	*armv7*)
+		filemanager_arch="armv7";;
+	*)
+		green "Aborted, unsupported or unknown architecture: $unamem"
+		return 2
+		;;
+	esac
+
+	unameu="$(tr '[:lower:]' '[:upper:]' <<<$(uname))"
+	if [[ $unameu == *DARWIN* ]]; then
+		filemanager_os="darwin"
+	elif [[ $unameu == *LINUX* ]]; then
+		filemanager_os="linux"
+	elif [[ $unameu == *FREEBSD* ]]; then
+		filemanager_os="freebsd"
+	elif [[ $unameu == *NETBSD* ]]; then
+		filemanager_os="netbsd"
+	elif [[ $unameu == *OPENBSD* ]]; then
+		filemanager_os="openbsd"
+	elif [[ $unameu == *WIN* || $unameu == MSYS* ]]; then
+		# Should catch cygwin
+		sudo_cmd=""
+		filemanager_os="windows"
+		filemanager_bin="filebrowser.exe"
+		filemanager_dl_ext=".zip"
+	else
+		green "Aborted, unsupported or unknown OS: $uname"
+		return 6
+	fi
+	green "Downloading File Browser for $filemanager_os/$filemanager_arch..."
+	if type -p curl >/dev/null 2>&1; then
+		net_getter="curl -fsSL"
+	elif type -p wget >/dev/null 2>&1; then
+		net_getter="wget -qO-"
+	else
+		green "Aborted, could not find curl or wget"
+		return 7
+	fi
+	filemanager_file="${filemanager_os}-$filemanager_arch-filebrowser$filemanager_dl_ext"
+	filemanager_tag="$(${net_getter}  https://api.github.com/repos/filebrowser/filebrowser/releases/latest | grep -o '"tag_name": ".*"' | sed 's/"//g' | sed 's/tag_name: //g')"
+	filemanager_url="${proxy}https://github.com/filebrowser/filebrowser/releases/download/$filemanager_tag/$filemanager_file"
+	echo "$filemanager_url"
+
+	# Use $PREFIX for compatibility with Termux on Android
+	rm -rf "$PREFIX/tmp/$filemanager_file"
+
+	${net_getter} "$filemanager_url" > "$PREFIX/tmp/$filemanager_file"
+
+	green "Extracting..."
+	case "$filemanager_file" in
+		*.zip)    unzip -o "$PREFIX/tmp/$filemanager_file" "$filemanager_bin" -d "$PREFIX/tmp/" ;;
+		*.tar.gz) tar -xzf "$PREFIX/tmp/$filemanager_file" -C "$PREFIX/tmp/" "$filemanager_bin" ;;
+	esac
+	chmod +x "$PREFIX/tmp/$filemanager_bin"
+
+	green "Putting filemanager in $install_path (may require password)"
+	$sudo_cmd mv "$PREFIX/tmp/$filemanager_bin" "$install_path/$filemanager_bin"
+	if setcap_cmd=$(PATH+=$PATH:/sbin type -p setcap); then
+		$sudo_cmd $setcap_cmd cap_net_bind_service=+ep "$install_path/$filemanager_bin"
+	fi
+	$sudo_cmd rm -- "$PREFIX/tmp/$filemanager_file"
+
+	if type -p $filemanager_bin >/dev/null 2>&1; then
+		green "Successfully installed"
+		trap ERR
+		return 0
+	else
+		red "Something went wrong, File Browser is not in your path"
+		trap ERR
+		return 1
+	fi
 }
 
 # 启动文件管理器
