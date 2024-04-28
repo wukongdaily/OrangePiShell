64998edccb546e3310f0dd640a70b6da8ee0f0f8
wukongdaily <2666180@gmail.com>
2024-04-28 09:23:31 +0800

    Update pi.sh

--- zero3/pi.sh
+++ zero3/pi.sh
@@ -234,6 +234,7 @@ install_xiaoya_alist() {
         red "检测到没有安装小雅必备依赖 AList"
         green "正在安装alist 请稍后"
         docker run -d --restart=unless-stopped -v /etc/alist:/opt/alist/data -p 5244:5244 -e PUID=0 -e PGID=0 -e UMASK=022 --name="alist" xhofe/alist:latest
+        sleep 3
         docker exec -it alist ./alist admin set admin
         echo '
     AList已安装,现在你可以安装小雅了
